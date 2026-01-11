f48b5346da9c08311526672a6ed5e49a8b8bf8d5
ClarkWilliamsIE <clark@inertiaed.org>
2026-01-11 14:31:05 +1300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // ADD THIS LINE: This tells the app it's living in a subfolder on GitHub
+      base: '/Homedash/', 
+      
       server: {
         port: 3000,
         host: '0.0.0.0',
